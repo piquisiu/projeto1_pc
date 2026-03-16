14fcb3f3f6e5371ef856cd853bb8281f642968e9
piquisiu <maiackshg@gmail.com>
2026-03-16 00:52:22 -0300

    Atualizado para parte final do codigo

--- PyInvest.py
+++ PyInvest.py
@@ -45,4 +45,38 @@ montante_lci = (cp * m.pow((1 + taxa_lci), ms) + (ap * ms))
 taxa_poupanca = 0.005
 montante_poupanca = (cp * m.pow((1+taxa_poupanca),ms) + (ap * ms))
 
-#FII - SIMULAÇÕES
+#FII
+base = cp * m.pow((1+t),ms) + (ap*ms)
+s1 = base * (1 + r.uniform(-0.03,0.03))
+s2 = base * (1 + r.uniform(-0.03,0.03))
+s3 = base * (1 + r.uniform(-0.03,0.03))
+s4 = base * (1 + r.uniform(-0.03,0.03))
+s5 = base * (1 + r.uniform(-0.03,0.03))
+
+media = s.mean((s1,s2,s3,s4,s5))
+mediana = s.median((s1,s2,s3,s4,s5))
+desvio = s.stdev((s1,s2,s3,s4,s5))
+
+#datas
+hoje = d.datetime.now()
+data_resgate = hoje + d.timedelta(days=ms*30)
+
+print("Simulador de Investimentos")
+print("Data da simulação:", hoje.strftime('%d/%m/%Y'))
+print("Data estimada de resgate:", data_resgate.strftime('%d/%m/%Y'))
+print("Total investido:", l.currency(total_investido, grouping=True))
+
+print("RESULTADOS FINANCEIROS")
+print("CDB:", l.currency(montante_cdb_liquido, grouping=True))
+print('#' * int(montante_cdb_liquido//1000))
+print("LCI:", l.currency(montante_lci, grouping=True))
+print('#' * int(montante_lci//1000))
+print("Poupança:", l.currency(montante_poupanca, grouping=True))
+print('#' * int(montante_poupanca//1000))
+print("FII (média):", l.currency(media, grouping=True))
+print('#' * int(media//1000))
+
+print("ESTATÍSTICAS FII")
+print("Mediana:", l.currency(mediana, grouping=True))
+print("Desvio padrão:", l.currency(desvio, grouping=True))
+print("Meta atingida?", media >= mt)
